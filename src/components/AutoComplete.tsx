ed975b11228dfeafcdf4b165f572f7ba38a4e0e5
danielrz <drazafinjato@gmail.com>
2023-10-03 16:31:47 +0300

    remove the use of useEffect in component

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,21 +1,16 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Props } from "../interfaces/autoComplete";
 import useSearch from "../hooks/useSearch";
 
 function AutoComplete({ interval }: Props) {
-  const [items, setItems] = useState<string[]>([]);
   const [term, setTerm] = useState<string>("");
 
-  const searchItems = useSearch({ term, interval });
+  const items = useSearch({ term, interval });
 
   async function onItemType(event: ChangeEvent<HTMLInputElement>) {
     const term = event.target.value;
     setTerm(term);
   }
-
-  useEffect(() => {
-    setItems(searchItems);
-  }, [searchItems]);
 
   return (
     <>
