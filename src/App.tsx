0d6cfdfffb43107fcaed8bb4917ce0b7d4468a7c
danielrz <drazafinjato@gmail.com>
2023-10-05 10:25:48 +0300

    few files for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { ReactNode, useContext, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { AnotherVerySlowComponent, VerySlowComponent } from './components/very-slow-component';
+import './styles.scss';
 
+const Context = React.createContext({
+  isNavExpanded: false,
+  toggle: () => {},
+});
+
+const NavigationController = ({ children }: { children: ReactNode }) => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  const toggle = () => setIsNavExpanded(!isNavExpanded);
+  const value = { isNavExpanded, toggle };
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+const useNavigation = () => useContext(Context);
+
+const AdjustableColumnsBlock = () => {
+  const { isNavExpanded } = useNavigation();
+  return isNavExpanded ? <div>two block items here</div> : <div>three block items here</div>;
+};
+
+const MainPart = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <VerySlowComponent />
+      <AnotherVerySlowComponent />
+      <AdjustableColumnsBlock />
     </>
-  )
-}
+  );
+};
 
-export default App
+const ExpandButton = () => {
+  const { isNavExpanded, toggle } = useNavigation();
+
+  return <button onClick={toggle}>{isNavExpanded ? 'collapse <' : 'expand >'}</button>;
+};
+
+const SidebarLayout = ({ children }: { children: ReactNode }) => {
+  const { isNavExpanded } = useNavigation();
+  return (
+    <div className="left" style={{ flexBasis: isNavExpanded ? '50%' : '20%' }}>
+      {children}
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <SidebarLayout>
+      {/* this one will control the expand/collapse */}
+      <ExpandButton />
+
+      <ul>
+        <li>
+          <a href="#">some links</a>
+        </li>
+      </ul>
+    </SidebarLayout>
+  );
+};
+
+const Layout = ({ children }: { children: ReactNode }) => <div className="three-layout">{children}</div>;
+
+const Page = () => {
+  return (
+    <NavigationController>
+      <Layout>
+        <Sidebar />
+        <MainPart />
+      </Layout>
+    </NavigationController>
+  );
+};
+
+export default function App() {
+  return (
+    <>
+      <h3>Very slow "Page" component - click on expand/collapse to toggle nav</h3>
+      <p>Only components that use Context are now re-render</p>
+      <p>Expand/collapse of the navigation doesn't trigger re-render of the slow components anymore, because of Context</p>
+      <Page />
+    </>
+  );
+}
